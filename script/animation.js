522e5141cd17085127424bb2fdc3273d0ebb2098
Danila <samsonchik2406@mail.ru>
2022-06-03 13:37:53 +0300

    second commit

---
+++ script/animation.js
@@ -0,0 +1,18 @@
+
+// var gallery_block = document.querySelector('.gallery');
+
+// var new_added_blocks = `<div class="card">
+//     <img src="img/gpu.jpg" alt="gpu" class="gpu-card">
+//     <h2 class="name-card">“gpu”</h2>
+//     <p class="descr-card">GPU from Shapes Collection</p>
+//     </div>`;
+
+// gallery_block.addEventListener("mouseenter", function(){
+//     gallery_block.innerHTML += new_added_blocks;
+// });
+
+// gallery_block.addEventListener("mouseover", function(){
+//     gallery_block.innerHTML += new_added_blocks;
+// });
+
+
